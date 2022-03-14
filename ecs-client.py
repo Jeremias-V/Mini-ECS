89f1c7a75057f8b605332bcbe1ffdbcfe7f9ce2b
Jeremias-V <jeremias.villalobos.t@gmail.com>
2022-03-13 19:42:25 -0500

    Create, List, Stop and Delete socket signal working.

--- ecs-client.py
+++ ecs-client.py
@@ -3,11 +3,19 @@ from time import sleep
 
 def main():
     connection = miniecs.MiniECS()
-    print(connection.create_container("Test_1"))
-    print(connection.create_container("Test_2"))
-    print(connection.create_container("Test_3"))
+    print(connection.create_container("Ubuntu"))
+    print(connection.create_container("Fedora"))
+    print(connection.create_container("Mint"))
     sleep(5)
-    print(connection.create_container("Test_4"))
+    print(connection.create_container("Kali"))
+    print(connection.list_containers())
+    print(connection.stop_container("Mint"))
+    print()
+    print(connection.list_containers())
+    print(connection.delete_instance("Fedora"))
+    print()
+    print(connection.list_containers())
+
     connection.stop()
 
 main()
